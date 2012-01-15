e24030faefe350eba0519a2dd8ae440578eec04d
Gaz Davidson <gaz@bitplane.net>
2012-01-15 22:54:54 +0000

    Return something when it fails, printing "NaN" to the console.

--- temper.c
+++ temper.c
@@ -6,13 +6,20 @@
 #include "pcsensor.h"
 
 int main(){
+
 	usb_dev_handle* lvr_winusb = pcsensor_open();
-	if (!lvr_winusb)
+
+	if (!lvr_winusb) {
+		printf("NaN");
+		fflush(stdout);
+
 		return -1;
+	}
 
 	float tempc = pcsensor_get_temperature(lvr_winusb);
 	pcsensor_close(lvr_winusb);
 	printf("%f", tempc);
 	fflush(stdout);
+
 	return 0;
 }
